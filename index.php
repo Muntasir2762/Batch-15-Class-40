f9f3c2a6edc4f7542fac9190ee83f85685bd64e3
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-11-13 23:47:20 +0600

    Switch-Case & function

---
+++ index.php
@@ -0,0 +1,74 @@
+
+
+<?php
+
+// $marks = 89;
+
+// if($marks>=80){
+//     echo "The Grade is A+";
+// }
+
+// elseif($marks>=70){
+//     echo "The grade is A";
+// }
+
+
+// switch($marks){
+
+//     case 80;
+//     echo "The grade is A+";
+//     break;
+
+//     case 70;
+//     echo "The grade is A";
+//     break;
+
+//     case 60;
+//     echo "The grade is A-";
+//     break;
+
+//     default:
+//     echo "The grade is F";
+//     break;
+// }
+
+
+// $rahim = "sjgj";
+
+// switch($rahim){
+//     case "car";
+//     echo "Long tour";
+//     break;
+
+//     case "bike";
+//     echo "Hang out";
+//     break;
+
+//     case "cycle";
+//     echo "Field";
+//     break;
+
+//     default:
+//     echo "stay home";
+//     break;
+// }
+
+
+function sum ($number1, $number2){
+    $x = $number1;
+    $y = $number2;
+
+    $result = $x+$y;
+    // echo $result;
+    return $result;
+}
+
+$sumValue = sum (56, 60); //116
+
+$number3 = 200;
+
+$secondSumValue = $sumValue+$number3;
+
+echo $secondSumValue;
+
+?>
